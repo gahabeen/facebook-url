e4caed347bca8deef6abf50447d13fade6d93b61
Gabin Desserprit <desserprit.gabin@gmail.com>
2022-01-25 20:59:54 +0100

    Added helper

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const parseResources = require('./lib/parse');
 const craftResources = require('./lib/craft');
+const datasetsAccessors = require('./lib/datasets');
 
 module.exports = {
     ...parseResources,
     ...craftResources,
+    ...datasetsAccessors,
 };
